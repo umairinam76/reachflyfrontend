b9d73a95deb373c22be38b62d5d396f3095d7acd
umairinam <umairinam76@gmail.com>
2026-08-05 21:23:48 +0500

    Fix:Frontend fix

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ import SeoLanding from "./pages/SeoLanding";
 /*
  * Authentication pages
  */
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login";
+import Signup from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 
